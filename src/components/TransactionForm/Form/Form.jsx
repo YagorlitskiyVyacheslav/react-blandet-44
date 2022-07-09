3afd71e78c4dfe897f4270e593bb47e9b9c9f54b
Tatianayarchenko <ktanya61@gmail.com>
2022-07-09 12:19:37 +0300

    task 4

--- src/components/TransactionForm/Form/Form.jsx
+++ src/components/TransactionForm/Form/Form.jsx
@@ -1,28 +1,47 @@
-import React, { Component } from "react";
-import { Formik, Form as FormikForm, Field } from "formik";
-import * as yup from "yup";
-import { PrimaryButton } from "../../ui/buttons";
-
+import React, { Component } from 'react';
+// import Select from 'react-select';
+import { Formik, Form as FormikForm } from 'formik';
+import * as yup from 'yup';
+import { PrimaryButton } from '../../ui/buttons';
+import { FormikInput } from '../../ui/inputs';
+import { FormikSelect } from '../../ui/inputs/FormikSelect';
+import { currencies } from '../../../constants';
 
 export class Form extends Component {
-  validationSchema = yup.object().shape(
-    {amount: yup.string().required("This field is required")}
-  )
+  validationSchema = yup.object().shape({
+    amount: yup.string().required('This field is required'),
+    fee: yup.string().required('This field is required'),
+  });
 
+  options = [
+    {
+      label: currencies.USD,
+      value: currencies.USD,
+    },
+    {
+      label: currencies.UAN,
+      value: currencies.UAN,
+    },
+  ];
 
   render() {
     return (
       <Formik
-      initialValues={{amount: ""}}
-      onSubmit={(values) => console.log(values)}
-      validationSchema={this.validationSchema}
+        initialValues={{ amount: '', fee: '' }}
+        onSubmit={(values) => console.log(values)}
+        validationSchema={this.validationSchema}
       >
-        {props => (
-        <FormikForm>
-          <Field name="amount" type="input" value={props.values.amount} onChange={props.handleChange}/>
-          <PrimaryButton type="submit">Add {this.props.type}</PrimaryButton>
-        </FormikForm>)}
-        
+        {({ setFieldValue, values }) => (
+          <FormikForm>
+            <FormikInput name="amount" type="input" />
+            <FormikSelect
+              field={{ name: 'fee' }}
+              form={{ setFieldValue, value: values.fee }}
+              options={this.options}
+            />
+            <PrimaryButton type="submit">Add {this.props.type}</PrimaryButton>
+          </FormikForm>
+        )}
       </Formik>
     );
   }
--- src/mock/transactions.js
+++ src/mock/transactions.js
@@ -1,4 +1,4 @@
-import { transactionType } from "../constants/transactionType";
+import { transactionType, currencies } from '../constants';
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default {
@@ -8,35 +8,35 @@ export default {
       type: transactionType.DEPOSIT,
       amount: 5000,
       created: 1656225673188,
-      fee: "USD",
+      fee: currencies.USD,
     },
     {
       id: 2,
       type: transactionType.WITHDRAW,
       amount: 3000,
       created: 1656225673188,
-      fee: "USD",
+      fee: currencies.USD,
     },
     {
       id: 3,
       type: transactionType.DEPOSIT,
       amount: 7000,
       created: 1656225673188,
-      fee: "UAN",
+      fee: currencies.UAN,
     },
     {
       id: 4,
       type: transactionType.DEPOSIT,
       amount: 2000,
       created: 1656225673188,
-      fee: "USD",
+      fee: currencies.USD,
     },
     {
       id: 5,
       type: transactionType.WITHDRAW,
       amount: 1000,
       created: 1656225673188,
-      fee: "UAN",
+      fee: currencies.UAN,
     },
   ],
   total: 10000,

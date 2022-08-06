fc504709f762168bb2ac403a4f722cb0f9aaadac
Tatianayarchenko <ktanya61@gmail.com>
2022-08-06 10:59:04 +0300

    tasc-10

--- src/index.js
+++ src/index.js
@@ -1,18 +1,22 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
-import { theme } from "./styleConfig/theme";
-import { ThemeProvider } from "styled-components";
-import { BrowserRouter } from "react-router-dom";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import { theme } from './styleConfig/theme';
+import { ThemeProvider } from 'styled-components';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from 'store';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <Provider store={store}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </Provider>
     </ThemeProvider>
   </React.StrictMode>
 );
